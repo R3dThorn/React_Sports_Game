b6fd2daee633d88af3620086f7356e0ca288fea6
Thorn <stachelrodtr4@gmail.com>
2020-07-26 17:16:54 -0500

    reset button added

--- main.js
+++ main.js
@@ -7,30 +7,30 @@ class Team extends React.Component {
         super(props)
         this.handleShot = this.handleShot.bind(this)
     }
-    
-    handleShot(){
+
+    handleShot() {
         const shootAttempt = new Audio("media/audio/woodbat.mp3")
         const scorePoint = new Audio("media/audio/Ball+Hit+Cheer.mp3")
         let score = this.props.score
         let shots = this.props.shots
 
-        if((Math.random()*10)>6){
+        if ((Math.random() * 10) > 6) {
             score++
             shots++
             scorePoint.play()
         }
-        else{
+        else {
             shots++
             shootAttempt.play()
         }
         this.props.handleGame(score, shots)
     }
-            
+
     render() {
         const ratio = (this.props.score / this.props.shots) || 0
         return (
             <div className={this.props.teamOrigin}>
-                <Scoreboard 
+                <Scoreboard
                     score={this.props.score}
                     team={this.props.teamOrigin}
                 />
@@ -38,14 +38,14 @@ class Team extends React.Component {
                 <img src={this.props.logo} />
                 <p>Score: {this.props.score}</p>
                 <p>Shots Taken: {this.props.shots}</p>
-                <p className={"_ratio"} style={{visibility: this.props.visibility}}>Shot Ratio: {(ratio*100).toFixed(3) + '%'}</p>
+                <p style={{ visibility: this.props.visibility }}>Shot Ratio: {(ratio * 100).toFixed(3) + '%'}</p>
                 <button onClick={this.handleShot}>Shoot!</button>
             </div>
         )
     }
 }
 
-class Game extends React.Component{
+class Game extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -53,24 +53,36 @@ class Game extends React.Component{
             homeShots: 0,
             homeVis: "hidden",
 
-            visitScore : 0,
-            visitShots : 0,
-            visitVis : "hidden"
+            visitScore: 0,
+            visitShots: 0,
+            visitVis: "hidden"
         }
         this.handleHome = this.handleHome.bind(this)
         this.handleVisit = this.handleVisit.bind(this)
+        this.handleReset = this.handleReset.bind(this)
     }
-    handleHome(score, shot){
-        this.setState({homeScore : score, homeShots : shot, homeVis : "visible"})
-    }
-
-    handleVisit(score, shot){
-        this.setState({visitScore : score, visitShots : shot, visitVis : "visible"})
+    handleHome(score, shots) {
+        this.setState({ homeScore: score, homeShots: shots, homeVis: "visible" })
     }
 
-    render(){
+    handleVisit(score, shots) {
+        this.setState({ visitScore: score, visitShots: shots, visitVis: "visible" })
+    }
+
+    handleReset() {
+        if(this.state.homeShots !== 0 || this.state.visitShots !== 0)
+            {alert("Game reset!")}
+        this.setState({
+            homeScore: 0, homeShots: 0, homeVis: "hidden",
+            visitScore: 0, visitShots: 0, visitVis: "hidden"
+        })
+    }
+
+    render() {
         return (
             <>
+                <button onClick={this.handleReset}>Reset Game</button>
+                <br />
                 <h1>Welcome to {this.props.venue}!</h1>
                 <div id="container">
                     <Team
@@ -97,8 +109,8 @@ class Game extends React.Component{
     }
 }
 
-function Scoreboard (props){
-    return(
+const Scoreboard = (props) => {
+    return (
         <>
             <h1>{props.team} Score</h1>
             <h1>{props.score}</h1>
