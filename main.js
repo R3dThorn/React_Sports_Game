94593969a36dd1513e172be1128526a20ff54003
Thorn <stachelrodtr4@gmail.com>
2020-07-27 15:29:50 -0500

    project fixed to suit rubric guidelines

--- main.js
+++ main.js
@@ -1,112 +1,5 @@
-function App(props) {
+const App = (props) => {
     return <Game venue='Appropriate Name Stadium' />
-}
-
-class Team extends React.Component {
-    constructor(props) {
-        super(props)
-        this.handleShot = this.handleShot.bind(this)
-    }
-
-    handleShot() {
-        const shootAttempt = new Audio("media/audio/woodbat.mp3")
-        const scorePoint = new Audio("media/audio/Ball+Hit+Cheer.mp3")
-        let score = this.props.score
-        let shots = this.props.shots
-
-        if ((Math.random() * 10) > 6) {
-            score++
-            shots++
-            scorePoint.play()
-        }
-        else {
-            shots++
-            shootAttempt.play()
-        }
-        this.props.handleGame(score, shots)
-    }
-
-    render() {
-        const ratio = (this.props.score / this.props.shots) || 0
-        return (
-            <div className={this.props.teamOrigin}>
-                <Scoreboard
-                    score={this.props.score}
-                    team={this.props.teamOrigin}
-                />
-                <h1>Team {this.props.name}</h1>
-                <img src={this.props.logo} />
-                <p>Score: {this.props.score}</p>
-                <p>Shots Taken: {this.props.shots}</p>
-                <p style={{ visibility: this.props.visibility }}>Shot Ratio: {(ratio * 100).toFixed(3) + '%'}</p>
-                <button onClick={this.handleShot}>Shoot!</button>
-            </div>
-        )
-    }
-}
-
-class Game extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            homeScore: 0,
-            homeShots: 0,
-            homeVis: "hidden",
-
-            visitScore: 0,
-            visitShots: 0,
-            visitVis: "hidden"
-        }
-        this.handleHome = this.handleHome.bind(this)
-        this.handleVisit = this.handleVisit.bind(this)
-        this.handleReset = this.handleReset.bind(this)
-    }
-    handleHome(score, shots) {
-        this.setState({ homeScore: score, homeShots: shots, homeVis: "visible" })
-    }
-
-    handleVisit(score, shots) {
-        this.setState({ visitScore: score, visitShots: shots, visitVis: "visible" })
-    }
-
-    handleReset() {
-        if(this.state.homeShots !== 0 || this.state.visitShots !== 0)
-            {alert("Game reset!")}
-        this.setState({
-            homeScore: 0, homeShots: 0, homeVis: "hidden",
-            visitScore: 0, visitShots: 0, visitVis: "hidden"
-        })
-    }
-
-    render() {
-        return (
-            <>
-                <button onClick={this.handleReset}>Reset Game</button>
-                <br />
-                <h1>Welcome to {this.props.venue}!</h1>
-                <div id="container">
-                    <Team
-                        teamOrigin="Home"
-                        name="SF Giants"
-                        logo="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/San_Francisco_Giants_Cap_Insignia.svg/800px-San_Francisco_Giants_Cap_Insignia.svg.png"
-                        handleGame={this.handleVisit}
-                        score={this.state.visitScore}
-                        shots={this.state.visitShots}
-                        visibility={this.state.visitVis}
-                    />
-                    <Team
-                        teamOrigin="Visiting"
-                        name="NY Yankees"
-                        logo="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/NewYorkYankees_caplogo.svg/800px-NewYorkYankees_caplogo.svg.png"
-                        handleGame={this.handleHome}
-                        score={this.state.homeScore}
-                        shots={this.state.homeShots}
-                        visibility={this.state.homeVis}
-                    />
-                </div>
-            </>
-        )
-    }
 }
 
 const Scoreboard = (props) => {
@@ -116,6 +9,127 @@ const Scoreboard = (props) => {
             <h1>{props.score}</h1>
         </>
     )
+}
+
+function Team(props) {
+    const ratio = (props.score / props.shots) || 0
+    return (
+        <div className={props.teamOrigin}>
+            <Scoreboard
+                score={props.score}
+                team={props.teamOrigin}
+            />
+            <h1>Team {props.name}</h1>
+            <img src={props.logo} />
+            <p>Score: {props.score}</p>
+            <p>Shots Taken: {props.shots}</p>
+            <p style={{ visibility: props.visibility }}>Shot Ratio: {(ratio * 100).toFixed(3) + '%'}</p>
+            <button onClick={props.handleGame}>Shoot!</button>
+        </div>
+    )
+}
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            homeStats: {
+                score: 0,
+                shots: 0,
+                vis: "hidden"
+            },
+
+            visitStats: {
+                score: 0,
+                shots: 0,
+                vis: "hidden"
+            },
+
+            resetCounter : 0
+        }
+        this.handleShot = this.handleShot.bind(this)
+        this.handleReset = this.handleReset.bind(this)
+    }
+
+    handleShot(team) {
+        const teamKey = `${team}Stats`
+        const teamScore = this.state[teamKey].score
+        const teamShots = this.state[teamKey].shots
+        const shootAttempt = new Audio("media/audio/woodbat.mp3")
+        const scorePoint = new Audio("media/audio/Ball+Hit+Cheer.mp3")
+
+        if ((Math.random() * 10) > 6) {
+            this.setState((state, props) => ({
+                [teamKey]: {
+                    score: teamScore + 1,
+                    shots: teamShots + 1,
+                    vis: "visible"
+                }
+            }))
+            scorePoint.play()
+        }
+        else {
+            this.setState((state, props) => ({
+                [teamKey]: {
+                    score: teamScore,
+                    shots: teamShots + 1,
+                    vis: "visible"
+                }
+            }))
+            shootAttempt.play()
+        }
+
+    }
+
+    handleReset() {
+        if (this.state.homeStats.shots !== 0 || this.state.visitStats.shots !== 0) { 
+            alert("Game reset!")
+            this.setState({resetCounter : this.state.resetCounter + 1})
+        }
+        this.setState({
+            homeStats: {
+                score: 0, 
+                shots: 0, 
+                vis: "hidden"},
+                
+            visitStats:{
+                score: 0, 
+                shots: 0, 
+                vis: "hidden"}
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <button onClick={this.handleReset}>Reset Game</button>
+                <br />
+                <span>Resets this session: {this.state.resetCounter}</span>
+                <br />
+                <h1>Welcome to {this.props.venue}!</h1>
+                <div id="container">
+                    <Team
+                        teamOrigin="Home"
+                        name="SF Giants"
+                        logo="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/San_Francisco_Giants_Cap_Insignia.svg/800px-San_Francisco_Giants_Cap_Insignia.svg.png"
+                        handleGame={() => this.handleShot("home")}
+                        score={this.state.homeStats.score}
+                        shots={this.state.homeStats.shots}
+                        visibility={this.state.homeStats.vis}
+                    />
+                    <Team
+                        teamOrigin="Visiting"
+                        name="NY Yankees"
+                        logo="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/NewYorkYankees_caplogo.svg/800px-NewYorkYankees_caplogo.svg.png"
+                        handleGame={() => this.handleShot("visit")}
+                        score={this.state.visitStats.score}
+                        shots={this.state.visitStats.shots}
+                        visibility={this.state.visitStats.vis}
+                    />
+                </div>
+            </>
+        )
+    }
 }
 
 ReactDOM.render(
